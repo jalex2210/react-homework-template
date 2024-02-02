69dbf48bcb12d9dfc21b96110157589e119f2a22
jalex2210 <alexbenitez.rdm@gmail.com>
2024-02-02 18:30:56 -0500

    tarea 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import user from '../json/user.json';
+import data from '../json/data.json';
+import transactions from '../json/transactions.json';
+import Profile from "./Profile.jsx";
+import Statistics from './Statistics.jsx';
+import FriendsList from './FriendList.jsx';
+import TransactionHistory from './TransactionHistory.jsx';
+
+
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        paddingBlockStart: '16px',
         display: 'flex',
+        flexDirection: 'column',
+        rowGap: '32px',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 41,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        avatar={user.avatar}
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title='UPLOAD STATS' stats={data} />
+      <Statistics stats={data} />
+      <FriendsList/>
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
